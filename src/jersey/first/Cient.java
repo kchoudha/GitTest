26998f8b7d33e359d359c68af7f0f29a4ed3c2b5
kchoudha <Kunal.Choudhary@symphonysv.com>
2012-03-02 10:18:29 +0530

    Update src/jersey/first/Cient.java

--- src/jersey/first/Cient.java
+++ src/jersey/first/Cient.java
@@ -32,9 +32,6 @@ public class Cient {
 				MediaType.TEXT_HTML).get(String.class));
 		System.out.println(" Here Kunal, Changed something");
 		System.out.println(" Here Manoja, Changed something, changing again");
-		System.out.println(" Here Manoja, Changed something, changing again");
-		System.out.println(" Here Manoja, Changed something, changing again");
-		System.out.println(" Here kunal choudhary again, Changed client file again,Changing again @again and @againa");
 		System.out.println(" Here kunal choudhary again, Changed client file again,Changing @again kunal @again again @again and @againa");
 
 	}
