d677f583f0e2f013cb8c38feda6c2abeb1e601ce
msahu <msahu@BD11215B.symphonysv.com>
2012-03-02 14:17:27 +0530

    testing conflict

--- src/jersey/first/Cient.java
+++ src/jersey/first/Cient.java
@@ -30,7 +30,7 @@ public class Cient {
 		System.out.println(service.path("rest").path("hello").accept(
 				MediaType.TEXT_HTML).get(String.class));
 		System.out.println(" Here Kunal, Changed something");
-		System.out.println(" Here Manoja, Changed something, changing again");
+		System.out.println(" Here Manoja, doing R&D to resolve conflict in git Hub......");
 	}
 
 	private static URI getBaseURI() {
